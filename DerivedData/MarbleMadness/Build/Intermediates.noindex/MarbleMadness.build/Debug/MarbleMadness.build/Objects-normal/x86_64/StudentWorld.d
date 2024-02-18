3d592d25e32a790d81955d8b9388589a22d3329b
Sunny Vinay <sunnyvinay7@gmail.com>
2024-02-18 14:43:01 -0800

    Update repo with .gitignore

--- DerivedData/MarbleMadness/Build/Intermediates.noindex/MarbleMadness.build/Debug/MarbleMadness.build/Objects-normal/x86_64/StudentWorld.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/sunny/uclawinter24/cs32/MarbleMadness/MarbleMadness/StudentWorld.cpp \
-  /Users/sunny/uclawinter24/cs32/MarbleMadness/MarbleMadness/StudentWorld.h \
-  /Users/sunny/uclawinter24/cs32/MarbleMadness/MarbleMadness/GameWorld.h \
-  /Users/sunny/uclawinter24/cs32/MarbleMadness/MarbleMadness/GameConstants.h \
-  /Users/sunny/uclawinter24/cs32/MarbleMadness/MarbleMadness/Level.h
